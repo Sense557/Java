f698bf2c9f922b3924ff33132eb7b7e3b8a96279
Sense <mehershakti1020@gmail.com>
2023-02-15 23:30:51 +0530

    Uploaded theory classes of pw java

---
+++ Operators & Loops/Assignment.java
@@ -0,0 +1,30 @@
+
+class Assignment
+{
+    public static void main(String [] args)
+    {
+        int m1, m2, m3, m4;
+
+        m1 = m2 = m3 = m4 = 10;
+
+        System.out.println(m1 + " " + m2 + " "+m3+ ' '+ m4);
+
+
+        //
+        int a = 10;
+        a += 5;
+        System.out.println(a);  //15
+
+        int b = 5;
+        b -= 3;
+        System.out.println(b);  //2
+
+        int c = 10;
+        c /= 5;
+        System.out.println(c);   //2
+
+        int d = 10;
+        d *= 2;
+        System.out.println(d);  //20
+    }
+}
---
+++ Operators & Loops/Relationall.java
@@ -0,0 +1,18 @@
+class Main
+{
+    public static void main(String[] args) 
+  {
+
+        //Relationall operator
+        int a = 10;
+        // int b = 10;
+        int c = 20;
+
+        boolean result = a<c;
+        System.out.println(result);  //true
+        System.out.println(a>c);  // false
+
+        // System.out.println("Hii"); //Hii
+      
+    }
+}
