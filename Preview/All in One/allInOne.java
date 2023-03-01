d4182f5ba9e6801f05f76fece53323fd0a1da9aa
Sense <mehershakti1020@gmail.com>
2023-03-01 23:29:00 +0530

    New all in one file built for revidion all in one go

---
+++ Preview/All in One/allInOne.java
@@ -0,0 +1,36 @@
+
+//🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕
+/**
+ * Encapsulation
+ *      -Data hiding + Data Abstraction
+ *
+ */
+
+
+/**
+ *  Normal class building and object creation
+ */
+
+class Student
+{
+    String name;
+    int age;
+
+    void show()
+    {
+        System.out.println(name+" "+age);
+    }
+}
+
+
+public class allInOne {
+    public static void main(String[] args) {
+
+        Student obj = new Student();
+        obj.show();
+
+    }
+}
+
+
+//🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕
