7c7437c2a7ab9f21753449e507d91303895fba6c
Sense <mehershakti1020@gmail.com>
2023-03-22 18:34:07 +0530

    modified

--- Milestone III Started/Practice/index4.java
+++ Milestone III Started/Practice/index4.java
@@ -1,43 +1,43 @@
-package Practice;
+// package Practice;
 
-import java.util.TreeSet;
+// import java.util.TreeSet;
 
-/**
- *  TreeSet
- *      Note-
- *          - Duplicates not allowed
- *          - Binary Search is fast
- *          - Sorted Order
- */
+// /**
+//  *  TreeSet
+//  *      Note-
+//  *          - Duplicates not allowed
+//  *          - Binary Search is fast
+//  *          - Sorted Order
+//  */
 
 
-public class index4 
-{
-    public static void main(String[] args) 
-    {
-        TreeSet ts = new TreeSet();
-        ts.add(100);
-        ts.add(200);
-        ts.add(300);
-        ts.add(25);
-        ts.add(30);
-        ts.add(80);
-        ts.add(90);
+// public class index4 
+// {
+//     public static void main(String[] args) 
+//     {
+//         TreeSet ts = new TreeSet();
+//         ts.add(100);
+//         ts.add(200);
+//         ts.add(300);
+//         ts.add(25);
+//         ts.add(30);
+//         ts.add(80);
+//         ts.add(90);
         
         
 
-        System.out.println(ts.ceiling(10));
-        System.out.println(ts.floor(20));
-        System.out.println(ts.higher(50));
-        System.out.println(ts.lower(30));
-        System.out.println(ts.add(100));
-        // ts.add(); //error
-        System.out.println(ts);
+//         System.out.println(ts.ceiling(10));
+//         System.out.println(ts.floor(20));
+//         System.out.println(ts.higher(50));
+//         System.out.println(ts.lower(30));
+//         System.out.println(ts.add(100));
+//         // ts.add(); //error
+//         System.out.println(ts);
 
-        // character  and string is not allowed
-        // ts.add("Java");
+//         // character  and string is not allowed
+//         // ts.add("Java");
 
-        System.out.println(ts);
+//         System.out.println(ts);
 
-    }
-}
+//     }
+// }
