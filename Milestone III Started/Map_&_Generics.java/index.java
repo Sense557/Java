095e5f9bc308aafa857ab4829b5894aa0ea0633d
Sense <mehershakti1020@gmail.com>
2023-03-28 21:48:24 +0530

    Modified

---
+++ Milestone III Started/Map_&_Generics.java/index.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/**
+ *  Hierarchy of Map
+ */
+
+
+
+
+public class index 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello");
+
+        Hashtable h = new Hashtable();
+        HashMap hm = new HashMap();
+        LinkedHashMap lhm = new LinkedHashMap();
+        TreeMap tm = new TreeMap();
+
+        
+    }
+}
