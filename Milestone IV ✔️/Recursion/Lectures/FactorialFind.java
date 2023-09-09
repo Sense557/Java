ea9a823480fe376d55a036edb0a553ed0e2f38c5
Sense <104638287+Sense557@users.noreply.github.com>
2023-09-09 23:14:25 +0530

    Update and rename factorialFind.java to FactorialFind.java

--- Milestone IV ✔️/Recursion/Lectures/FactorialFind.java
+++ Milestone IV ✔️/Recursion/Lectures/FactorialFind.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class factorialFind {
+class actorialFind {
     // function definition
     public static int factorialFinding(int n){
         int result = 0;
