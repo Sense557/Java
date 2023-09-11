d263818c8e748430c14f5cb4a38fa6a202e6bfd5
Sense <104638287+Sense557@users.noreply.github.com>
2023-09-11 23:48:37 +0530

    Create FindMiddleData.java

---
+++ Milestone V ✔️/LinkedList/Practice/FindMiddleData.java
@@ -0,0 +1,72 @@
+class LinkedList {
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    public int findMiddleData() {
+        if (head == null) {
+            throw new IllegalStateException("The linked list is empty.");
+        }
+
+        Node slowPointer = head;
+        Node fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        return slowPointer.data;
+    }
+
+    public void displayList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+
+        System.out.println("Original Linked List:");
+        list.displayList();
+
+        int middleData = list.findMiddleData();
+        System.out.println("Middle Data: " + middleData);
+    }
+}
