9a046f9d7f5e0d6db09f57fbd2e06ce9c7f9ebd5
Sense <104638287+Sense557@users.noreply.github.com>
2023-09-17 22:53:58 +0530

    Create CanCompleteCircuit.java

---
+++ Milestone V ✔️/Greedy Approach/Interview Q/CanCompleteCircuit.java
@@ -0,0 +1,22 @@
+public class CanCompleteCircuit {
+    public static int canCompleteCircuit(int[] A, int[] B) {
+        int fuel =0, sum=0, start = 0;
+        for(int i = 0; i<A.length; i++){
+            sum += (A[i] - B[i]);
+            fuel += (A[i] - B[i]);
+            if(fuel < 0){
+                fuel = 0;
+                start = i+1;
+            }
+        }
+        if(sum>=0){
+            return start % (A.length);
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int [] A = {1,2};
+        int [] B = {2,1};
+        System.out.println(canCompleteCircuit(A,B));
+    }
+}
